4ca465c9f19aa52bbe93d2952cb496a8ad402552
Vladimir Migashko <migashko@gmail.com>
2014-04-01 20:30:53 +0400

    upd

--- leveldbd/leveldb.cpp
+++ leveldbd/leveldb.cpp
@@ -6,8 +6,10 @@
 #include <hub/hub_module.hpp>
 #include "build_info.h"
 #include <core/build_info.h>
-
 #include <module/leveldb_module.hpp>
+#include <pubsub_service/pubsub_service_module.hpp>
+#include <pubsub_gateway/pubsub_gateway_module.hpp>
+
 
 
 int main(int argc, char* argv[])
@@ -20,6 +22,8 @@ int main(int argc, char* argv[])
       { "config",  std::make_shared< wfc::config_module >() },
       { "logger",  std::make_shared< wfc::logger_module >() }, 
       { "hub",     std::make_shared< wfc::hub_module    >() }, 
+      { "pubsub-service",     std::make_shared< wfc::pubsub_service_module >() },
+      { "pubsub-gateway",     std::make_shared< wfc::pubsub_gateway_module >() }, 
       { "leveldb",    std::make_shared< wamba::leveldb::leveldb_module >() }
     }
   ).run(argc, argv);
