1e929f146725cc98912341e47036effd7425bda1
Vladimir Migashko <migashko@gmail.com>
2014-04-11 00:46:38 +0400

    upd

---
+++ tests/config.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+#server=("cdaemon11", 22500)
+#server=("ldb2", 22550)
+#server=("127.0.0.1", 22345)
+#server=("cdaemon11", 32500)
+server=("127.0.0.1", 12345)
+#server=("127.0.0.1", 12345)
+#server=("127.0.0.1", 12347)
+#server=("127.0.0.1", 12348)
+#server=("cdaemon16", 22510)
+# test master
+#server=("ctest2", 22500)
+# test slave
+#server=("ctest1", 22510)
+#server=("ctest1", 22520)
