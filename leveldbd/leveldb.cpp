9fbb4f03b3955d68b5614300d5566e992a3e2be1
Vladimir Migashko <migashko@gmail.com>
2015-06-19 16:21:30 +0300

    wfc-0.6 upd

--- leveldbd/leveldb.cpp
+++ leveldbd/leveldb.cpp
@@ -1,14 +1,15 @@
-#include "demod_build_info.h"
+#include "leveldbd_build_info.h"
 
 #include <wfc/wfc.hpp>
 #include <wfcroot/wfcroot.hpp>
-#include <wfc_demo/demo/demo_package.hpp>
+#include <wfc_leveldb/leveldb/leveldb_package.hpp>
 
 int main(int argc, char* argv[])
 {
   return ::wfc::wfc(
-    ::wfc::make_build_info<demod_build_info>(), 
+    ::wfc::make_build_info<leveldbd_build_info>(), 
     ::wfc::wfcroot({
+      std::make_shared< ::wamba::leveldb::leveldb_package >()
     })
   ).run(argc, argv);
 }
