ef5c6b318ed3756aa952210c5d0a869281d5f4f4
Vladimir Migashko <migashko@gmail.com>
2015-06-19 13:16:23 +0300

    initial 0.6

--- leveldbd/leveldb.cpp
+++ leveldbd/leveldb.cpp
@@ -1,32 +1,14 @@
-#include <wfc/wfc.hpp>
-#include <startup/startup_module.hpp>
-#include <core/core_module.hpp>
-#include <config/config_module.hpp>
-#include <logger/logger_module.hpp>
-#include <hub/hub_module.hpp>
-#include <repli_hub/repli_hub_module.hpp>
-#include <pubsub_service/pubsub_service_module.hpp>
-#include <pubsub_gateway/pubsub_gateway_module.hpp>
-//#include <core/build_info.h>
-#include <module/leveldb_module.hpp>
-#include "leveldbd_build_info.h"
-          
+#include "demod_build_info.h"
 
+#include <wfc/wfc.hpp>
+#include <wfcroot/wfcroot.hpp>
+#include <wfc_demo/demo/demo_package.hpp>
 
 int main(int argc, char* argv[])
 {
-  return wfc::wfc(
-    leveldbd_build_info_string,
-    {
-      { "startup", std::make_shared< wfc::startup_module>() },
-      { "core",    std::make_shared< wfc::core_module   >() },
-      { "config",  std::make_shared< wfc::config_module >() },
-      { "logger",  std::make_shared< wfc::logger_module >() }, 
-      { "hub",     std::make_shared< wfc::hub_module    >() },
-      { "repli-hub",       std::make_shared< wfc::repli_hub_module >()      },
-      { "pubsub-service",  std::make_shared< wfc::pubsub_service_module >() },
-      { "pubsub-gateway",  std::make_shared< wfc::pubsub_gateway_module >() }, 
-      { "leveldb",    std::make_shared< wamba::leveldb::leveldb_module >()  }
-    }
+  return ::wfc::wfc(
+    ::wfc::make_build_info<demod_build_info>(), 
+    ::wfc::wfcroot({
+    })
   ).run(argc, argv);
 }
