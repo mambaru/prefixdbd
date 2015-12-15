c76806a676a2c8758401e703242a7c280488ed6a
Vladimir Migashko <migashko@gmail.com>
2015-12-15 20:23:28 +0300

    initial prefixdb

--- leveldbd/leveldb.cpp
+++ leveldbd/leveldb.cpp
@@ -2,13 +2,15 @@
 
 #include <wfc/wfc.hpp>
 #include <wfcroot/wfcroot.hpp>
-#include <wfc_leveldb/leveldb/leveldb_package.hpp>
+#include <leveldb/leveldb_package.hpp>
+#include <prefixdb/prefixdb_package.hpp>
 
 int main(int argc, char* argv[])
 {
   return ::wfc::wfc<leveldbd_build_info>(::wfc::wfcroot(
     {
-      std::make_shared< ::wamba::leveldb::leveldb_package >()
+      std::make_shared< ::wamba::leveldb::leveldb_package >(),
+      std::make_shared< ::wamba::prefixdb::prefixdb_package >()
     }
   )).run(argc, argv);
 }
