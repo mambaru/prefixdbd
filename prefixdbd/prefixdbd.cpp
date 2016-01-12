22578e66544d9229d60cfd4f1b74761b6f1a45ba
Vladimir Migashko <migashko@gmail.com>
2016-01-12 23:00:11 +0300

    *CMake

--- prefixdbd/prefixdbd.cpp
+++ prefixdbd/prefixdbd.cpp
@@ -1,15 +1,13 @@
-#include "leveldbd_build_info.h"
+#include "prefixdbd_build_info.h"
 
 #include <wfc/wfc.hpp>
 #include <wfcroot/wfcroot.hpp>
-#include <leveldb/leveldb_package.hpp>
 #include <prefixdb/prefixdb_package.hpp>
 
 int main(int argc, char* argv[])
 {
-  return ::wfc::wfc<leveldbd_build_info>(::wfc::wfcroot(
+  return ::wfc::wfc<prefixdbd_build_info>(::wfc::wfcroot(
     {
-      std::make_shared< ::wamba::leveldb::leveldb_package >(),
       std::make_shared< ::wamba::prefixdb::prefixdb_package >()
     }
   )).run(argc, argv);
