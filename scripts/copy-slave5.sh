6b52fcafe8ad722ae1b549e1eb0fab71202ebdbb
Vladimir Migashko <migashko@gmail.com>
2016-06-22 17:32:04 +0300

    conf upd

---
+++ scripts/copy-slave5.sh
@@ -0,0 +1,20 @@
+echo "***************************************************************"
+echo "** запуск с полным копированием с мастера cdaemon25:master3  **"
+echo "***************************************************************"
+
+addr=ldb2
+port=23553
+
+master=master3
+slave=slave3
+
+user=`whoami`
+src=$user@$addr:/monamour/prefixdb/$master/*
+dst=/monamour/prefixdb/$slave/
+
+sudo chown -R $user:users $dst
+rm -f -r $dst
+(echo "db 600"; sleep 1) | telnet $addr $port 2> /dev/null
+rsync  --progress -rh $src $dst
+(echo "cb"; sleep 1) | telnet $addr $port 2> /dev/null
+sudo chown -R wwwrun:www $dst
